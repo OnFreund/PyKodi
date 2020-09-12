e5c7b0e7eebe069f2e02768c371381a4eaa87411
On Freund <onfreund@gmail.com>
2020-09-12 08:55:38 +0300

    Bump jsonrpc deps

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ AUTHOR = 'On Freund'
 REQUIRES_PYTHON = '>=3.7.0'
 VERSION = '0.1.3'
 
-REQUIRED = ['jsonrpc-base', 'jsonrpc-async', 'jsonrpc-websocket', 'aiohttp']
+REQUIRED = ['jsonrpc-async>=1.1.0', 'jsonrpc-websocket>=1.2.1', 'aiohttp']
 
 EXTRAS = {}
 
